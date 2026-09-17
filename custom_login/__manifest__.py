61ba7f211699804333cf5cb746309f8a647d5467
Diptiranjan <diptiranjansahu999@gmail.com>
2026-09-17 15:19:36 +0530

    feat : add login page custom UI

---
+++ custom_login/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Custom Login',
+    'version': '1.0',
+    'category': 'Customizations',
+    'summary': 'Custom login branding and layout',
+    'author': 'Techvizor',
+    'depends': ['web'],
+    'data': [
+        'views/login_templates.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
